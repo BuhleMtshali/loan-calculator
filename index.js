3b322e2b92081691bb7a8a3fa13d318bb7221724
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-09-06 22:16:24 +0200

    updated the index file

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ let loanAmount = document.getElementById("loan-amount");
 let interestRate = document.getElementById("interest-rate");
 let monthsElement = document.getElementById("months");
 let outputElement = document.getElementById("output-amount");
-let buttonElement = document.getElementById("output-amount");
+let buttonElement = document.getElementById("calculate-btn");
 let clearAllElement = document.getElementById("clear-all");
+
+//calculate function
+buttonElement.addEventListener("click", calculateNow);
+function calculateNow() {
+  let interest = (interestRate.value / 100) * loanAmount.value;
+  let totalAmount =
+    (interest + parseFloat(loanAmount.value)) / monthsElement.value;
+  outputElement.textContent = totalAmount;
+}
