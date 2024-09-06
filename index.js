2f906e4b851d90e612ee669443322ab3e8453067
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-09-06 21:58:11 +0200

    updated the javascript file

---
+++ index.js
@@ -0,0 +1,6 @@
+let loanAmount = document.getElementById("loan-amount");
+let interestRate = document.getElementById("interest-rate");
+let monthsElement = document.getElementById("months");
+let outputElement = document.getElementById("output-amount");
+let buttonElement = document.getElementById("output-amount");
+let clearAllElement = document.getElementById("clear-all");
